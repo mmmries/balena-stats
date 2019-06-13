bde48f4575bb43bcd9f8124fe21f50bc8fda1a0f
mmmries <riesmmm@gmail.com>
2019-06-12 21:23:27 -0600

    just grafana/influx

--- sensor/entry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-mkdir -p /data/sensor
-touch /data/sensor/bsec_iaq.state
-cp -n /usr/src/app/bsec_bme680_linux/bsec_iaq.config /data/sensor/bsec_iaq.config
-
-python /usr/src/app/scripts/take_measurement.py
